4fdabe580b7ab12bcf047bda7b6106b5766ee1e0
Pratham Verma <110521546+PrathamVerma999@users.noreply.github.com>
2023-11-09 21:30:04 +0530

    Added High score functionality

--- main.py
+++ main.py
@@ -33,20 +33,20 @@ while game_is_on:
     # Detecting Collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.update_score()
+        score.increase_score()
         snake.extend()
 
     
     # Detecting collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
     
     # Detect collision with tail     
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
         
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -15,19 +15,33 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(y=270, x=0)
         
-        self.score_count = -1
+        self.score_count = 0
+        with open("data.txt") as file:
+            self.high_score = file.read()
         self.color("white")
-        self.write(arg=f"Score: {self.score_count}", move=False, align=ALIGNMENT, font=FONT)
-        self.update_score()
+        self.update_scoreboard()
         
-       
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align = ALIGNMENT, font= FONT)
-    
-        
-    def update_score(self):
-        self.score_count += 1
+    def update_scoreboard(self):
         self.clear()
-        self.write(arg=f"Score: {self.score_count}", move=False, align="center", font=("Arial", 20, "normal"))
+        self.write(arg=f"Score: {self.score_count} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+        
+    def reset(self):
+        
+        
+        self.high_score = int(self.high_score)
+        
+        if self.score_count > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score_count))
+        
+        with open("data.txt") as file:
+            self.high_score = file.read()
+        
+            
+        self.score_count = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score_count += 1
+        self.update_scoreboard()
             
